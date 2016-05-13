caf2bdb6f6caaf5acc02b6496f54270390c3b40e
Mohan Naidu <mohannaidu@mohans-air.speedport_w_921v_1_39_000>
2016-05-13 10:29:27 +0200

    replace old code

--- Exercise_3/fk.js
+++ Exercise_3/fk.js
@@ -1,8 +1,37 @@
-//load fs module from Node core library
-var fs = require('fs');
-//get path to file
-var path = process.argv[2];
-/* read file, returning Buffer object containing file contents;
-Then convert to string & split into array of lines */
-var data = fs.readFileSync(path).toString().split('\n');
+// initialisation
+
+
+
+    var fs;
+    var path;
+    var data;
+    //load fs module from Node core library
+    fs = require('fs');
+    //get path to file
+    path = process.argv[2];
+    read = fs.readFileSync(path);
+
+
+
+    data = read.toString().split('\n');
+    fs.open(data, 'r+', function(err, fd) {
+   if (err) {
+       return console.error(err);
+       console.log("no file");
+   }
+  console.log("File opened successfully!");
+});
+    /*    if (err) {
+    console.error('There was an error reading the file!', err);
+    return "there is a error";
+} */
+  // Otherwise handle the data
+
+
+
+    /* read file, returning Buffer object containing file contents;
+    Then convert to string & split into array of lines */
+
+
+
 console.log(data.length-1);
