635aa1b5899e3e93adc623364231a8584b8ff3cd
Mohan Naidu <mohannaidu@mohans-air.speedport_w_921v_1_39_000>
2016-05-13 10:35:48 +0200

    add final solution

--- Exercise_3/Sync_Fileoperations.js
+++ Exercise_3/Sync_Fileoperations.js
@@ -1,37 +1,31 @@
 // initialisation
+var fs;
+var path;
+var data;
+var util = require('util');
 
+//load fs module from Node core library
+fs = require('fs');
+//get path to file
+path = (process.argv[2]) || ""; //default is an empty string
 
+if (path === '' && typeof path === "undefined") {
+    console.log("path must be a string or Buffer");
+    return;
+}
 
-    var fs;
-    var path;
-    var data;
-    //load fs module from Node core library
-    fs = require('fs');
-    //get path to file
-    path = process.argv[2];
-    read = fs.readFileSync(path);
-
-
-
-    data = read.toString().split('\n');
-    fs.open(data, 'r+', function(err, fd) {
-   if (err) {
-       return console.error(err);
-       console.log("no file");
-   }
-  console.log("File opened successfully!");
-});
-    /*    if (err) {
-    console.error('There was an error reading the file!', err);
-    return "there is a error";
-} */
-  // Otherwise handle the data
-
-
-
-    /* read file, returning Buffer object containing file contents;
-    Then convert to string & split into array of lines */
-
-
-
-console.log(data.length-1);
+/* read file, returning Buffer object containing file contents;
+Then convert to string & split into array of lines */
+try{
+    data = fs.readFileSync(path).toString().split('\n');
+    console.log(data.length-1);
+}
+catch(err){
+    if (err.code === 'ENOENT') {
+      console.log('File not found!');
+    } else {
+        console.log(
+            "An technical error occured."
+        );
+    }
+}
